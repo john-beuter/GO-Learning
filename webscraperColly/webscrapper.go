ba56c6a2574a65276694c51df1202914ed6e34de
Johnathon Beuter <jbeuter@iastate.edu>
2024-03-15 22:13:48 -0500

    Building a general webscrapper

--- webscraperColly/webscrapper.go
+++ webscraperColly/webscrapper.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gocolly/colly" //I downloaded you though?
+)
+
 func main() {
-	println("hello, world")
+	c := colly.NewCollector(
+		colly.AllowedDomains("en.wikipedia.org"),
+	)
+
+	//Scraping HTML
+	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
+		links := e.ChildAttr("a", "href")
+		fmt.Println(links)
+	})
 }
