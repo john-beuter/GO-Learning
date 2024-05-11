8f1f423085d5c84a6ef147bb8e7c22957d87ceff
John <johnathonbeuter@gmail.com>
2024-05-11 14:59:13 -0500

    Making progress on Gophercise 1

--- GOphercise1/excercise1.go
+++ GOphercise1/excercise1.go
@@ -1,8 +1,45 @@
 package main
 
+
+//Need to read in values from a CSV and make it a game.
+
 import(
+  "encoding/csv" 
   "fmt"
+  "io"
+  "log"
+  "os"
 )
+
 func main() {
-  fmt.Print("hello") 
+
+  f, err := os.Open("problems.csv")
+  if err != nil{
+    log.Fatal(err)
+  }
+
+
+  defer f.Close()
+
+  csvReader := csv.NewReader(f)
+  for{
+    rec, err := csvReader.Read()
+    if err == io.EOF{
+      break
+    }
+
+    if err != nil{
+       log.Fatal(err)
+    }
+    
+    fmt.Println("%v", rec[0])
+    fmt.Println("Enter you answer: ")
+
+    //Need to add a reading to get the users input
+    //then compare that input ot rec[1] if it matches have points
+
+
+  }
+
+
 }
